af64c6af322a46759f4d0d817900e9dc63971477
Daniil Alekseev <daniaalekseew@yandex.ru>
2022-12-06 16:01:42 +0300

    Header files for simulation classes

---
+++ NeuralNetwork.h
@@ -0,0 +1,6 @@
+//DO EVERYTHING HERE
+
+class NeuralNetwork final {
+    NeuralNetwork();
+    NeuralNetwork(const &NeuralNetwork base);
+}
---
+++ Simulation.h
@@ -0,0 +1,8 @@
+#include "Graphics.h"
+#include "Creature.h"
+
+class Simulation final {
+private:
+    unsigned int simulationSizeX, simulationSizeY, simulationSizeZ;
+public:
+}
