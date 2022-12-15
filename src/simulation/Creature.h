0652965cbb5a0f8334c350dee83991876172b9c1
ZmMatvey <115418268+ZmMatvey@users.noreply.github.com>
2022-12-15 21:21:10 +0300

    Add files via upload

--- src/simulation/Creature.h
+++ src/simulation/Creature.h
@@ -1,57 +1,76 @@
-#ifndef MEMORY
+#ifndef CREATURE_H
+#define CREATURE_H
+
 #include <memory>
-#endif
-
-#ifndef VECTOR
 #include <vector>
-#endif
-
-#ifndef MATH
 #include <cmath>
-#endif
 
-#include "neuralnetwork/NeuralNetwork.h"
+#include "../neuralnetwork/NeuralNetwork.h"
 
-class Creature {
+class Creature 
+{
 private:
-    float x, y, z, angleXY, angleYZ;
+    float x, y, z, angleXY, angleYZ; 
+
     const float fovXY;
     const float fovYZ;
+
     const unsigned int numberOfRaysXY;
     const unsigned int numberOfRaysYZ;
+
     NeuralNetwork brain;
+
     const float MAXENERGY = 500.0f;
     const float MAXVELOCITY = 1;
     const float MAXANGULARVELOCITY = 0.04f;
+
     float energy = MAXENERGY;
     float birthCounter = 0.0f;
     float lastSpeed = 0.0f;
+
     const float hungerFactor;
+
     float timer = 1.0f;
+
 public:
     Creature(const float x, const float y, const float z,
         const float angleXY, const float angleYZ,
         const float fovXY, const float fovYZ,
         const unsigned int numberOfRaysXY, const unsigned int numberOfRaysYZ, const float hungerInput);
+
     Creature(const Creature& base);
+
     ~Creature();
+
     Creature& operator=(const Creature& base);
+
     NeuralNetwork brainCopy() const;
+
     float getX() const;
     float getY() const;
     float getZ() const;
+
     float getAngleXY() const;
     float getAngleYZ() const;
+
     float getFovXY() const;
     float getFovYZ() const;
+
     unsigned int getNumberOfRaysXY() const;
     unsigned int getNumberOfRaysYZ() const;
+
     void move(std::vector<float> &brainInput);
+
     float getEnergy() const;
     void setEnergy(float energyInput);
+
     float getBirthCounter() const;
     void setBirthCounter(float birthCounterInput);
+
     float getHungerFactor() const;
+
     float getTimer() const;
     void setTimer(const float t);
 };
+
+#endif //CREATURE_H
--- src/simulation/Simulation.h
+++ src/simulation/Simulation.h
@@ -1,19 +1,22 @@
-#include "simulation/Creature.h"
+#ifndef SIMULATION_H
+#define SIMULATION_H
 
-#ifndef ARRAY
+#include "Creature.h"
+
 #include <array>
-#endif // !ARRAY
-
-#ifndef VECTOR
 #include <vector>
-#endif
 
-class Simulation final {
+class Simulation final 
+{
 private:
     unsigned int simulationSizeX, simulationSizeY, simulationSizeZ;
+
     std::vector<Creature> herbivore;
     std::vector<Creature> carnivore;
+
 public:
     Simulation(unsigned int x, unsigned int y, unsigned int z);
     
 };
+
+#endif //SIMULATION_H
